1929bcd7ff35d6910bfeb482c595d213f11bb198
Denis <payar3282@gmail.com>
2022-07-09 23:04:55 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Navbar from "./components/Navbar"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  )
 }
 
-export default App;
+export default App
